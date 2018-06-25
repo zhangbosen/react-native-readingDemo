d71543fcc96b098a727f7507ebd765fbf09b5101
张森 <475306196@qq.com>
2018-06-25 16:17:08 +0800

    嵌入高德地图

--- app/park/ZSPark.js
+++ app/park/ZSPark.js
@@ -4,31 +4,20 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View
+    View,
+    WebView
 } from 'react-native';
 
 //引入工具
 import Util from '../util/Util';
 
-//引入导航
-import ZSCommonNav from '../util/ZSCommonNav';
 
-
-export default class App extends Component<Props> {
+export default class ZSPark extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <ZSCommonNav
-                    leftTitle="扫一扫"
-                    rightTitle="设置"
-                    mainTitle="公园"
-                    clickLeftView={() => {alert("点击了左边")}}
-                    clickRightView={() => {alert("点击了右边")}}
-                />
-                <Text style={styles.welcome}>
-                    公园页面
-                </Text>
-            </View>
+            <WebView
+                source={require('./html/park.html')}
+            />
         );
     }
 }
